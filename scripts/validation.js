2baf4e4041ba779e4f20b2b077dc508c69ae3cee
Alice <devo4ka2410@gmail.com>
2022-02-17 14:27:32 +0300

    feat: add validation
    add form validation via using regular expressions
    
    MODULE-1-PART-3-js-web-basics

---
+++ scripts/validation.js
@@ -0,0 +1,48 @@
+const email = document.getElementById('email');
+const errorEmail = document.getElementById('email-error');
+const password = document.getElementById('password');
+const errorPassword = document.getElementById('password-error');
+console.log (email);
+
+email.addEventListener( 'input', function () { 
+
+    console.log (email.value);
+    if ((/\w[-\w+]@\w+\.\w{2,}/.test(email.value))) {
+        email.classList.remove('invalid');
+        email.classList.add('valid');
+        errorEmail.innerHTML = 'Email корректен.'
+        errorEmail.classList.remove('authorization-form__error--invalid');
+        errorEmail.classList.add('authorization-form__error--valid');
+    } else {
+        email.classList.remove('valid');
+        email.classList.add('invalid');
+        errorEmail.innerHTML = 'Введите корректный email.'
+        errorEmail.classList.remove('authorization-form__error--valid');
+        errorEmail.classList.add('authorization-form__error--invalid');
+        console.log ('invalid');
+    }
+
+})
+
+password.addEventListener( 'input', function () { 
+
+    console.log (password.value);
+    if ((/[a-zA-Z0-9]{8,}/.test(password.value))) {
+        password.classList.remove('invalid');
+        password.classList.add('valid');
+        errorPassword.innerHTML = 'Пароль верен.'
+        errorPassword.classList.remove('authorization-form__error--invalid');
+        errorPassword.classList.add('authorization-form__error--valid');
+    } else {
+        password.classList.remove('valid');
+        password.classList.add('invalid');
+        errorPassword.innerHTML = 'Введите верный пароль.'
+        errorPassword.classList.remove('authorization-form__error--valid');
+        errorPassword.classList.add('authorization-form__error--invalid');
+        console.log ('invalid');
+    }
+
+   
+})
+    
+    
